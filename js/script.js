f6e9cd6c14cf41b49126e931fef3b84dac627349
ThellaVamshidhar <thellavamshidhar244@gmail.com>
2023-09-13 00:26:58 +0530

    initial commit
    First upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed (".typing", {
+    strings: ["FullStack Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    lopp: true
+})
---
+++ js/style-switcher.js
@@ -0,0 +1,55 @@
+// toggle style switcher
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+// hide style-switcher on scroll
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+// theme color
+// style-switcher.js
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color){
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        } else {
+            style.setAttribute("disabled", "true");
+        }
+    });
+}
+// theme light and dark mode
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
+
+//highlight menu
+
+function highlightMenuItem(clickedItem) {
+    // Remove the 'active' class from all menu items
+    var menuItems = document.querySelectorAll('.nav li a');
+    menuItems.forEach(function(item) {
+        item.classList.remove('active');
+    });
+
+    // Add the 'active' class to the clicked menu item
+    clickedItem.classList.add('active');
+}
