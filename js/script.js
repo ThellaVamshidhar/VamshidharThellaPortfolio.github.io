4dd4c2b4c802691491eb7af43b8666acbdd69aee
ThellaVamshidhar <96279293+ThellaVamshidhar@users.noreply.github.com>
2024-03-21 00:18:00 +0530

    Update script.js
    added fronend in script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // typing animation
 var typed = new Typed (".typing", {
-    strings: ["FullStack Developer"],
+    strings: ["Frontend Developer"],
     typeSpeed: 100,
     BackSpeed: 60,
     lopp: true
